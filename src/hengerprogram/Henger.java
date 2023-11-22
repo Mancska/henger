55c64dfd9a51b98086e4716e6b11035468b0fd5c
Mancska <95695531+Mancska@users.noreply.github.com>
2023-11-22 13:26:05 +0100

    osztályok

---
+++ src/hengerprogram/Henger.java
@@ -0,0 +1,38 @@
+
+package hengerprogram;
+public class Henger {
+    private static int hengerDarab;
+    private double sugar,magassag;
+
+    public Henger(double sugar, double magassag) {
+        this.sugar = sugar;
+        this.magassag = magassag;
+    }
+
+    public static void setHengerDarab(int hengerDarab) {
+        Henger.hengerDarab = hengerDarab;
+    }
+
+    public static int getHengerDarab() {
+        return hengerDarab;
+    }
+
+    public double getSugar() {
+        return sugar;
+    }
+
+    public double getMagassag() {
+        return magassag;
+    }
+
+    public double terfogat(){
+        
+        return 0;
+        
+    }
+    @Override
+    public String toString() {
+        return "Henger{" + "sugar=" + sugar + ", magassag=" + magassag + '}';
+    }
+    
+}
---
+++ src/hengerprogram/Hengerprogram.java
@@ -0,0 +1,17 @@
+
+package hengerprogram;
+
+/**
+ *
+ * @author FranyóKrisztián(SZF_
+ */
+public class Hengerprogram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
